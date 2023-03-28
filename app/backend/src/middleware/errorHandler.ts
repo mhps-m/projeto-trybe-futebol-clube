538a46e4b0f095ff5f81db852eedaeda2dbb8564
Miguel <miguelanjohps@gmail.com>
2023-03-28 20:53:17 -0300

    feat: Cria métodos para lidar com errors Cria construtor de erros que recebe um status code HTTP e um middleware para lidar com erros nas requisições

---
+++ app/backend/src/middleware/errorHandler.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from 'express';
+import HttpError from '../utils/HttpError';
+
+export default function errorHandler(
+  err: HttpError | Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+): Response {
+  return res
+    .status((err as HttpError).status || 500)
+    .json({ message: err.message });
+}
