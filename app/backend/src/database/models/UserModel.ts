392277e6d5dfe01c1461c8bca97fed80f3468612
Miguel <miguelanjohps@gmail.com>
2023-03-29 09:39:38 -0300

    feat: Conclui requisito 06 Cria model para tabela users

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,41 @@
+import { STRING, INTEGER, Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default User;
